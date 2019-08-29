1e12b773ade844829f51c1fcea6e73cb3304301c
kattakapilkumar <kapilkatta123@gmail.com>
2019-08-30 01:29:33 +0530

    initial host

---
+++ precache-manifest.97e3cd8f785df3201a8271d6fb06fb84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8fbd2663b1f83404f1643f367b09e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a698ca07e1724983f096",
+    "url": "/static/css/main.ed4553fa.chunk.css"
+  },
+  {
+    "revision": "577d9fe9c83f723592a7",
+    "url": "/static/js/2.593dd093.chunk.js"
+  },
+  {
+    "revision": "a698ca07e1724983f096",
+    "url": "/static/js/main.9329b116.chunk.js"
+  },
+  {
+    "revision": "49f5f6e0351ef0d39340",
+    "url": "/static/js/runtime~main.07489cd9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
